5887ee4e4a344687e015b2126b7d0955383b93ee
unknown <sanel8782@gmail.com>
2026-09-22 23:36:19 -0500

    add logica producto

--- src/main/java/com/lideratec/Producto.java
+++ src/main/java/com/lideratec/Producto.java
@@ -1,4 +1,37 @@
 package com.lideratec;
 
 public class Producto {
+
+    String nombre;
+    double precio;
+    int stock;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Precio: "+ precio);
+        System.out.println("Stock: "+ stock);
+
+    }
+
+    public void aumentarStock(int cantidad) {
+        stock = stock + cantidad;
+        System.out.println("Nuevo stock "+ stock);
+    }
+
+    public void reducirStock(int cantidad) {
+        if (cantidad > 0 && cantidad <= stock) {
+            stock = stock - cantidad;
+            System.out.println("Nuevo stock reducido "+ stock);
+        } else {
+            System.out.println("Cantidad inválida o stock insuficiente.");
+        }
+    }
+
+
 }
